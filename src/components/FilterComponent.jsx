79f5fba808340d0c859dd61272547e8e362a711e
Seng-Kimnam <sengkimnam97@gmail.com>
2025-03-20 13:56:08 +0700

    final commit

--- src/components/FilterComponent.jsx
+++ src/components/FilterComponent.jsx
@@ -12,40 +12,52 @@ import {
 import { usePathname, useRouter } from "next/navigation";
 
 const FilterComponent = ({ data }) => {
-  const [selectedName, setSelectedName] = React.useState(""); // Store selected name
   const router = useRouter();
   const pathname = usePathname();
+  const path =
+    pathname === "/book-category" ? "All Books" : "Old School Cartoons";
+  const [selectedName, setSelectedName] = React.useState(path); // Default title
+
   const handleSelect = (id) => {
     // Find the selected category by its id
     const selectedItem = data.find((item) => item.id === id);
 
-    const path =
-      pathname === "/book-category" ? "/book-category" : "/old-school-cartoons";
-    const type = path === "/book-category" ? "query" : "genre";
-    router.push(`${path}?${type}=${selectedItem.id}`);
     if (selectedItem) {
       setSelectedName(
         selectedItem.book_cate_name || selectedItem.cartoon_genre
       );
+      const segment =
+        pathname === "/book-category"
+          ? "/book-category"
+          : "/old-school-cartoons";
+      const type = segment === "/book-category" ? "query" : "genre";
+      router.push(`${segment}?${type}=${selectedItem.id}`);
     }
   };
 
   return (
-    <Select onValueChange={handleSelect}>
-      <SelectTrigger className="w-[280px]">
-        <SelectValue placeholder={selectedName || "Filter By Categories"} />
-      </SelectTrigger>
-      <SelectContent>
-        <SelectGroup>
-          <SelectLabel>Category</SelectLabel>
-          {data?.map((element) => (
-            <SelectItem key={element.id} value={element.id}>
-              {element.book_cate_name || element.cartoon_genre}
-            </SelectItem>
-          ))}
-        </SelectGroup>
-      </SelectContent>
-    </Select>
+    <>
+      {/* Dynamic Title */}
+      <p className="cyan-text bg-similar-white rounded-xl font-bold inline-block px-8 py-4 ml-4 mt-2">
+        {selectedName}
+      </p>
+
+      <Select onValueChange={handleSelect}>
+        <SelectTrigger className="w-[280px]">
+          <SelectValue placeholder="Select a Category" />
+        </SelectTrigger>
+        <SelectContent>
+          <SelectGroup>
+            <SelectLabel>Category</SelectLabel>
+            {data?.map((element) => (
+              <SelectItem key={element.id} value={element.id}>
+                {element.book_cate_name || element.cartoon_genre}
+              </SelectItem>
+            ))}
+          </SelectGroup>
+        </SelectContent>
+      </Select>
+    </>
   );
 };
 
--- src/components/HeaderContent.jsx
+++ src/components/HeaderContent.jsx
@@ -1,20 +1,10 @@
-"use client";
 import FilterComponent from "./FilterComponent";
 
-const HeaderContent = ({ pathname, data }) => {
+const HeaderContent = ({ data }) => {
   return (
     <article>
       <div className="flex justify-between">
-        <p className="cyan-text  bg-similar-white rounded-xl font-bold inline-block px-8 py-4 ml-4  mt-2">
-          {pathname === "/"
-            ? "Homepage"
-            : pathname === "/book-category"
-            ? "All Books"
-            : pathname === "/old-school-cartoons"
-            ? "Old School Cartoons"
-            : ""}
-        </p>
-        {pathname === "/" ? "" : <FilterComponent data={data} />}
+        <FilterComponent data={data} />
       </div>
       <hr className="w-[96%] flex m-0 mx-auto mt-4 cyan-text" />
     </article>
